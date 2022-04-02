cd3457bc21d92974ddc5184bdb4a15f4f23521ad
felimera <feli041089@gmail.com>
2022-04-02 10:25:05 -0500

    Organización de tests con JUnit

--- src/main/java/com/platzi/javatests/util/StringUtil.java
+++ src/main/java/com/platzi/javatests/util/StringUtil.java
@@ -2,6 +2,8 @@ package com.platzi.javatests.util;
 
 public class StringUtil {
     public static String repeat(String str, int times) {
+        if (times < 0)
+            throw new IllegalArgumentException("negative times not allowed");
         String resulta = "";
         for (int i = 0; i < times; i++) {
             resulta += str;
--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -5,8 +5,22 @@ import org.junit.Test;
 
 public class StringUtilTest {
     @Test
-    public void testRepeat() {
-        Assert.assertEquals("HolaHolaHola", StringUtil.repeat("Hola", 3));
+    public void repeat_string_one() {
         Assert.assertEquals("Hola", StringUtil.repeat("Hola", 1));
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("HolaHolaHola", StringUtil.repeat("Hola", 3));
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("Hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("Hola", -1);
     }
 }
