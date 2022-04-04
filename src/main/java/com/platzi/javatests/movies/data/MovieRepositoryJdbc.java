7655e296d760d73f0967893f8dd8a86108b3dad5
andres.mera <andres.mera@sitis.com.co>
2022-04-04 07:26:17 -0500

    Creación de la base de datos y tests de integración con bases de datos.

---
+++ src/main/java/com/platzi/javatests/movies/data/MovieRepositoryJdbc.java
@@ -0,0 +1,39 @@
+package com.platzi.javatests.movies.data;
+
+import com.platzi.javatests.movies.model.Genre;
+import com.platzi.javatests.movies.model.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.Collection;
+
+public class MovieRepositoryJdbc implements MovieRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public MovieRepositoryJdbc(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Movie findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+        return jdbcTemplate.query("select * from movies", movieMapper);
+    }
+
+    @Override
+    public void saveOrUpdate(Movie movie) {
+
+    }
+
+    private static RowMapper<Movie> movieMapper = (resultSet, i) -> new Movie(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getInt("minutes"),
+            Genre.valueOf(resultSet.getString("genre"))
+    );
+}
